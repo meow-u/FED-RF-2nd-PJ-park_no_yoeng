c61a228654736d280aec82e7241d58d53cf0a59d
noyeong <noyeong1994@gmail.com>
2024-03-15 18:19:41 +0900

    .

--- 1차프로젝트/03.구현소스/js/main.js
+++ 1차프로젝트/03.구현소스/js/main.js
@@ -1,7 +1,8 @@
 // 메인 js
-
-// 오버시 메뉴 보이기
+/* *********************************** */
+// [ 상단메뉴 오버시 서브메뉴 보이기 ]
 // 대상선정 : .gnb .menu
+/* *********************************** */
 
 let menu = document.querySelectorAll('.gnb .menu');
 
@@ -16,17 +17,22 @@ for(let i = 0; i<menu.length; i++){
         // 대상 높이값 지정하기
         tg.style.height = mh + 'px';
         tg.style.transition = '.4s ease-in-out';
-    };
+        
+    }; ///마우스 오버 이벤트함수///
+
     menu[i].onmouseleave = function(){
         let tg = this.querySelector('.submenu');
         // 대상 높이값 지정하기
         tg.style.height = '0px';
         tg.style.transition = '.4s ease-in-out';
-    };
+
+    };///마우스 떠날때 이벤트함수///
 
 } /// for ///
-
-// 중앙이미지만 오버시 .center 클래스 넣고 빼기
+/* ******************************************* */
+// [ 중앙이미지만 오버시 .center 클래스 넣고 빼기 ]
+// (밝아지는효과 적용된 클래스)
+/* ******************************************* */
 let visualView = document.querySelector('.visualView');
 let centerImg = document.querySelector('.img2');
 
@@ -36,3 +42,22 @@ visualView.onmouseenter = function(){
 visualView.onmouseleave = function(){
     centerImg.classList.add('center');
 };
+
+/******************************************** 
+ // [ 하단 ol오버시 상단메뉴 글씨색 바꾸기 ]
+ ********************************************/
+ // 이벤트대상: .sub-wrap .sub-wrap ol:first-of-type
+ // 변경대상: .gnb .menu a:first-of-type
+ let ol = document.querySelectorAll('.sub-wrap>ol');
+ let a = document.querySelectorAll('.gnb .menu>a:first-of-type')
+// console.log('대상확인:',ol,a);
+
+ for (let i = 0; i < ol.length; i++){
+    // console.log(ol[i]);
+    ol[i].onmouseenter = function(){
+        let tg1 = this.parentElement.querySelector('.menu>lia:first-of-type');
+        console.log(tg1);
+        /* 안됌!!!!!!타겟오류! */
+        // tg1.style.color ='white';
+    };
+ }

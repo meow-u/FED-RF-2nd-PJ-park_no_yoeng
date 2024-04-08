40bb3ca407c9c990746014dd72a885effafbb361
meow-u <noyeong1994@gmail.com>
2024-04-08 23:40:52 +0900

    돌아갈커밋

--- 1차프로젝트/03.구현소스/JS/make_html.js
+++ 1차프로젝트/03.구현소스/JS/make_html.js
@@ -1,4 +1,5 @@
-
+/* 내함수 js 가져오기 (새이름지음)*/
+import dFn from './my_function.js';
 // 데이터 셋팅 파일 불러오기★★ ///
 import * as sbData from "../data/sb_data.js";
 
@@ -12,21 +13,22 @@ export default function () {
 // ( ()=>{랩핑할코드} )();   <-즉시실행됨
 
 (()=>{ /////////코드 랩핑 ////////////
-const fcont = dFn.qs('.footer.cont2');
+const fcontBox = dFn.qs('.footer.cont2');
 console.log(sbData.footerData);
 
-let myhcode = '';
-sbData.footerData.forEach(v=>{
-  myhcode += `
-  <div class="footer ${v[0]}">${
-    v[1].map(v=>`<a href="#"><div>${v}</div></a>`).join('')
-  }</div>
+let hcode = '';
+sbData.footerData.forEach(v=>{ //여기서 v는  [ val, [val] ] 각각 총 4개
+  // console.log('v',v);
+  hcode += `
+  <div class="footer ${v[0]}">
+  ${v[1].map(v=>`<a href="#"><div>${v}</div></a>`).join('')}
+  </div>
 
   `
 })
 
-console.log(myhcode);
-fcont.innerHTML = myhcode;
+console.log(hcode);
+fcontBox.innerHTML = hcode;
 })();/////////코드 랩핑 ////////////
 
 

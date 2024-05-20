9e0a3d87a5e97b26e76775b9e5ec873a33e1c851
meow-u <noyeong1994@gmail.com>
2024-05-21 01:45:27 +0900

    .

---
+++ copy.js
@@ -0,0 +1,278 @@
+name: ["펜할리곤스 루나 EDT 30ML", "LUNA"]
+name: ["펜할리곤스 루나 EDT 100ML", "LUNA"]
+name: [
+name: ["펜할리곤스 루나 헤어퍼퓸 30ml", "LUNA"]
+name: ["펜할리곤스 바디&핸드로션 루나 500ML", "LUNA"]
+name: ["펜할리곤스 쥬니퍼 슬링 EDT 30ML", "JUNIPER SLING"]
+name: ["펜할리곤스 키스 오브 블리스 EDP 100ML", "A KISS OF BLISS"]
+name: ["펜할리곤스 더치스 로즈 EDP 75ML", "THE COVETED DUCHESS ROSE"]
+name: ["펜할리곤스 루나 핸드크림 75ML", "LUNA"]
+name: ["펜할리곤스 엠프레사 EDP 30ML", "EMPRESSA"]
+name: ["펜할리곤스 루나 바디 워시+로션 듀오세트", "LUNA"]
+name: ["펜할리곤스 바디&핸드워시 루나 500ML", "LUNA"]
+name: ["펜할리곤스 바디&핸드워시 엠프레사 500ML", "EMPRESSA"]
+name: ["펜할리곤스 바디&핸드로션 블렌하임 부케 500ML", "BLENHEIM BOUQUET"]
+name: ["펜할리곤스 솔라리스 EDP 30ML", "SOLARIS"]
+name: ["펜할리곤스 엘리자베단 로즈 EDP 30ML", "ELISABETHAN ROSE"]
+name: ["펜할리곤스 더 듀크 EDP 75ML", "MUCH ADO ABOUT THE DUKE"]
+name: ["펜할리곤스 블렌하임 부케 EDT 100ML", "BLENHEIM BOUQUET"]
+name: ["펜할리곤스 쥬니퍼 슬링 EDT 100ML", "JUNIPER SLING"]
+name: ["펜할리곤스 블렌하임 부케 EDT 30ML", "BLENHEIM BOUQUET"]
+name: ["펜할리곤스 쿼커스 핸드크림 75ML", "QUERCUS"]
+named
+name: ["펜할리곤스 엔드미온 EDC 30ML", "ENDYMION"]
+name: ["펜할리곤스 알울라 EDP 30ML", "PENHALIGON'S ALULA"]
+name: ["펜할리곤스 알울라 EDP 100ML", "PENHALIGON'S ALULA"]
+name: ["펜할리곤스 엔드미온 꽁상트레 EDP 30ML", "ENDYMION CONCENTRÉ"]
+name: ["펜할리곤스 미스터 톰슨 EDP 75ML", "THE OMNISCIENT MR THOMPSON"]
+name: ["펜할리곤스 포션 센트 라이브러리 2mlx5", "Potion Cent Library"]
+name: ["펜할리곤스 브라 브라 브룸 EDP 100ML", "VRA VRA VROOM"]
+name: ["펜할리곤스 오 디 오데시티 EDP 100ML", "EAU THE AUDACITY"]
+name: ["펜할리곤스 밤 오브 캄 EDP 100ML", "A BALM OF CALM"]
+name: ["펜할리곤스 리퀴드 러브 EDP 100ML", "LIQUID LOVE"]
+name: ["펜할리곤스 더 페이버릿 바디 워시+로션 듀오세트", "THE FAVOURITE"]
+name: [
+name: ["펜할리곤스 엠프레사 바디 워시+로션 듀오세트", "EMPRESSA"]
+name: ["펜할리곤스 엔드미온 바디 워시+로션 듀오세트", "ENDYMION"]
+name: ["펜할리곤스 쿼커스 바디 워시+로션 듀오세트", "QUERCUS"]
+named
+name: ["펜할리곤스 바디&핸드워시 블렌하임 부케 500ML", "BLENHEIM BOUQUET"]
+name: ["펜할리곤스 바디&핸드워시 더 페이버릿 500ML", "THE FAVOURITE"]
+name: ["펜할리곤스 바디&핸드워시 엔드미온 500ML", "ENDYMION"]
+name: ["펜할리곤스 바디&핸드워시 쿼커스 500ML", "QUERCUS"]
+named
+name: ["펜할리곤스 바디&핸드로션 더 페이버릿 500ML", "THE FAVOURITE"]
+name: [
+name: [
+name: [
+name: ["펜할리곤스 바디&핸드로션 쿼커스 500ML", "QUERCUS"]
+named
+name: ["펜할리곤스 바디&핸드로션 엔드미온 500ML", "ENDYMION"]
+name: ["펜할리곤스 바디&핸드로션 엠프레사 500ML", "EMPRESSA"]
+name: ["펜할리곤스 솔라리스 EDP 100ML", "SOLARIS"]
+name: ["펜할리곤스 스포츠카 클럽 EDP 100ML", "Sports Car Club"]
+name: ["펜할리곤스 로테어 EDT 100ML", "LOTHAIR"]
+name: ["펜할리곤스 아서 EDP 75ML", "THE WORLD ACCORDING TO ARTHUR"]
+name: ["펜할리곤스 레이디스 collection etc(5ml x 5)", "Ladies Collection"]
+name: [
+name: ["펜할리곤스 쿰간 로즈 캔들 200G", "KUMGAN ROSE"]
+name: ["펜할리곤스 헐트리스 헬렌 EDP 75ML", "HEARTLESS HELEN"]
+name: ["펜할리곤스 사토리얼 EDT 100ML", "SARTORIAL"]
+name: ["펜할리곤스 사보이 스팀 EDP 100ML", "SAVOY STEAM"]
+name: ["펜할리곤스 쿼커스 EDC 100ML", "QUERCUS"]
+named
+name: ["펜할리곤스 블래이징 미스터 샘 EDP 75ML", "THE BLAZING MISTER SAM"]
+name: ["펜할리곤스 바빌론 EDP 100ML", "BABYLON"]
+name: ["펜할리곤스 콘스탄티노플 EDP 100ml", "CONSTANTINOPLE"]
+name: ["펜할리곤스 엔드미온 꽁상트레 EDP 100ML", "ENDYMION CONCENTRÉ"]
+name: ["펜할리곤스 하맘 부케 EDT 100ml", "HAMMAM BOUQUET"]
+name: ["펜할리곤스 오퍼스 EDT 100ML", "OPUS 1870"]
+name: ["펜할리곤스 클레데스틴 클라라 EDP 75ML", "CLANDESTINE CLARA"]
+name: ["펜할리곤스 채인징 콘스탄스 EDP 75ML", "CHANGING CONSTANCE"]
+name: ["펜할리곤스 비위칭 야스민 EDP 75ML", "THE BEWITCHING YASMINE"]
+name: ["펜할리곤스 언컴프로마이징 소한 EDP 75ML", "UNCOMPROMISING SOHAN"]
+name: ["펜할리곤스 마두로 리프 캔들 200G", "MADURO LEAF"]
+name: ["펜할리곤스 포트레이트 맨션 10mlx8", "Portrait Mansion"]
+name: ["펜할리곤스 더 페이버릿 EDP 100ML", "THE FAVOURITE"]
+name: ["펜할리곤스 엠프레사 EDP 100ML", "EMPRESSA"]
+name: ["펜할리곤스 할페티 EDP 100ML", "HALFETI"]
+name: [
+name: ["펜할리곤스 레이디 블랑쉬 EDP 75ML", "THE REVENGE OF LADY BLANCHE"]
+name: ["펜할리곤스 테러블 테디 EDP 75ML", "TERRIBLE TEDDY"]
+name: ["펜할리곤스 릴리 오브 더 밸리 EDT 100ML", "LILY OF THE VALLEY"]
+name: ["펜할리곤스 오렌지 블라썸 EDT 100ML", "ORANGE BLOSSOM"]
+name: ["펜할리곤스 쿼커스 EDC 30ML", "QUERCUS"]
+named
+name: ["펜할리곤스 로드 조지 EDP 75ML", "THE TRAGEDY OF LORD GEORGE"]
+name: ["펜할리곤스 엘리자베단 로즈 EDP 100ML", "ELISABETHAN ROSE"]
+name: ["펜할리곤스 엔드미온 EDC 100ML", "ENDYMION"]
+name: ["펜할리곤스 아르테미지아 EDP 100ML", "ARTEMISIA"]
+name: ["펜할리곤스 오렌지 블라썸 EDT 30ML", "ORANGE BLOSSOM"]
+name: [
+name: ["펜할리곤스 더 페이버릿 핸드크림 75ML", "THE FAVOURITE"]
+name: ["펜할리곤스 아르테미지아 EDP 30ML", "ARTEMISIA"]
+name: ["펜할리곤스 더 페이버릿 EDP 30ML", "THE FAVOURITE"]
+
+
+PENHALIGON'S ALULA
+Across sweeping saffron deserts, an oasis of plum and patchouli perfumes the Incense Road.
+
+HALFETI
+So this is love. A mysterious perfume heady with rose, grapefruit and spice.
+
+THE COVETED DUCHESS ROSE
+A sweet-scented Rose, ready for the picking. Not so innocent after all.
+
+THE BLAZING MISTER SAM
+Hot and cold spices mingle over cedar and patchouli. Sam’s scent turns heads.
+
+THE TRAGEDY OF LORD GEORGE
+Rich, noble, deceptive. Like our patriarch, this woody perfume has secrets.
+
+THE FAVOURITE
+Mischief-tinged musk: this fragrance wields its own royally charming power.
+
+HALFETI LEATHER
+An icon reborn: bergamot, leather and oud mingle in the bazaar’s heady haze.
+
+CAIRO
+Damascena rose, patchouli and saffron perfume this ancient, sensual city.
+
+LUNA
+The Moon Goddess’ bath is perfumed with orange, jasmine and fir. Surrender.
+
+LIQUID LOVE
+A distillation of desire to set hearts a-racing.
+
+THE WORLD ACCORDING TO ARTHUR​
+An incensed (though, not like the rest!) warrior in a sweet- scented garden of sage wisdom and wit.
+
+EMPRESSA
+Radiant with peach, vanilla and blood orange, the Empressa’s scent bewitches.
+
+THE BEWITCHING YASMINE
+Will Yasmine’s scent of jasmine, incense and oud help her lure a suitable match?
+
+ENDYMION
+Bergamot dances off suede and geranium. An eau de cologne for the ages.
+
+QUERCUS
+Fresh zest and wet moss. A cologne – or a sunny day in the shade of an oak tree?
+
+A KISS OF BLISS
+An ointment for the optimist.
+
+THE OMNISCIENT MR THOMPSON
+A fragrance of a few, well-chosen words. Strong but subtle wood and leather, enveloped by vanilla and sesame milk.
+
+TERRIBLE TEDDY
+Incense, leather and ambroxan. A smooth and deadly operator.
+
+HIGHGROVE BOUQUET
+A floral hum of silver lime, mimosa and cedar. As radiant as the scent of earthly sunshine.
+
+SOLARIS
+A timeless ode to the Sun, radiant with a caress of white flowers. Dazzling citrus transforms like dappled light to blend with blackcurrant.
+
+EAU THE AUDACITY
+A confident concoction imbibed with bold incense. Does one dare?
+
+VRA VRA VROOM
+A vitality vapour infused with osmanthus.
+
+A BALM OF CALM
+Breathe in. Breathe out. Serenity, at last.
+
+PENHALIGON'S ALULA
+A mirage of plum and patchouli offer an oasis amid a saffron desert. Explore the Incense Road...
+
+HALFETI
+The romance of Halfeti made miniature. Rose, fruits and spice captured in a warm embrace.
+
+LUNA
+Voyage with the Moon Goddess, perfumed with orange, jasmine and fir. Surrender.
+
+EMPRESSA
+Radiant with peach, vanilla and blood orange, the Empressa’s scent bewitches.
+
+CAIRO
+Damascena rose, patchouli and saffron perfume this ancient, sensual city.
+
+THE FAVOURITE
+An aroma of great delicacy, golden mimosa, iris and musk perfumes the royal quarters.​
+
+ELISABETHAN ROSE
+The Tudor rose: symbol of Elizabethan England, immortalised in this airy scent.
+
+SOLARIS
+A timeless ode to the Sun, radiant with a caress of white flowers. Dazzling citrus transforms like dappled light to blend with blackcurrant.
+
+ENDYMION CONCENTRÉ
+Brave bergamot and strong suede make a heroic parfum – a miniature but mighty vessel of mythical proportion.
+
+JUNIPER SLING
+This eau de toilette is a gin lover’s delight. Better make it a double.
+
+BLENHEIM BOUQUET
+A cocktail of lemon, black pepper and pine, fresh and fragrant as British wit.
+
+BLUEBELL
+A woodland wander amidst a fragrant carpet of bluebells. Pure bliss.
+
+ELISABETHAN ROSE
+The Tudor rose: symbol of Elizabethan England, immortalised in this airy scent.
+
+THE INIMITABLE MR. PENHALIGON
+The Prince of Perfumes enters in a majestic haze of vetiver, as one would expect.​
+
+MUCH ADO ABOUT THE DUKE
+Rose, gin, leather; most uncommon. A scent to set Society tongues wagging.
+
+SARTORIAL
+Savile Row’s classic scent: steam, lavender, vanilla. Strictly for dapper gents.
+
+ENDYMION CONCENTRÉ
+Bergamot dances off suede, coffee and geranium. A scent for the ages.
+
+CHANGING CONSTANCE
+Cool cardamom, hot pimento, salted caramel – a scent with no regard for rules.
+
+PETRA
+A spicy-sweet scent swept away by desert winds. Myrrh longs for a city where warmth is no façade.
+
+JUNIPER SLING
+This eau de toilette is a gin lover’s delight. Better make it a double.
+
+OPUS 1870
+A masterpiece of yuzu and incense. A classic scent – never de rigueur.
+
+ARTEMISIA
+Fresh nectarine and jasmine sweetened by vanilla. A hymn sung on perfumed skin.
+
+ENDYMION
+Bergamot dances off suede and geranium. An eau de cologne for the ages.
+
+LILY OF THE VALLEY
+A timeless eau de toilette, as fresh and optimistic as the morning dew.
+
+LOTHAIR
+Scented with the spoils of adventure: juniper, fig milk and ambergris.
+
+HEARTLESS HELEN
+A flourish of tuberose in mandarin-scented woods. Hooked? You will be.
+
+ORANGE BLOSSOM
+A fragrant stroll through the honeyed haze of a sun-drenched orange grove.
+
+THE REVENGE OF LADY BLANCHE
+A picture of charm – or is she? This narcotic fragrance has a dangerous bite.
+
+CASTILE
+On a Spanish hillside, orange blossom and neroli spill their sun-born scent.
+
+SAVOY STEAM
+The perfectly perfumed hammam. Fresh Tunisian roses, incense and rosemary.
+
+DOURO
+A cologne with depth, drenched in bergamot, neroli and labdanum.
+
+ARTEMISIA
+Fresh nectarine and jasmine sweetened by vanilla. A melody on perfumed skin.
+
+QUERCUS
+Fresh zest and wet moss. A cologne – or a sunny day in the shade of an oak tree?
+
+HAMMAM BOUQUET
+Lavender and sandalwood rub shoulders in the fragrant Turkish baths of Mayfair.
+
+CONSTANTINOPLE
+A scent of the Queen of Cities. Florals meet earth, and worlds collide.
+
+BABYLON
+Love, mystery and majesty! A city fragrant with warm vanilla, cedar and saffron.
+
+CLANDESTINE CLARA
+Exotic, spicy, sweet-scented. Clara is a complex creature, impossible to resist.
+
+HALFETI CEDAR
+New heights of Halfeti. A river of rum and spice carve into mighty Cedar’s scent.

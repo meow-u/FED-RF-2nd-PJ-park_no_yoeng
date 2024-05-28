1761a967424fc7ce8e7c622e445aac56769f96d4
meow-u <noyeong1994@gmail.com>
2024-05-29 04:31:38 +0900

    .

--- 2차프로젝트/03.구현소스/js/components/banner.jsx
+++ 2차프로젝트/03.구현소스/js/components/banner.jsx
@@ -48,20 +48,20 @@ export default function Banner() {
           // 슬라이드 직전에 회전값, 스케일을 초기화하기
           // ( Index 값이 홀수/짝수 일때 회전방향 다르게 분기처리 )
             slideContent.style.transform = 
-            Index % 2 === 1? "rotate(5deg) scale(1)" : "rotate(-5deg) scale(1)";
+            Index % 2 === 1? "rotate(5deg) scale(1.2)" : "rotate(-5deg) scale(1.2)";
             slideContent.style.transition = "0s";
     
           // 슬라이드 전환시작시 스케일 키우기 (셋타임아웃을 주어서 초기화된 스케일값이 적용된 후에 실행되도록 설정)
           setTimeout(function () {
             slideContent.style.transition = "transform 0.5s ease-in-out";
-            slideContent.style.transform = "rotate(0deg) scale(1.2)";
+            slideContent.style.transform = "rotate(0deg) scale(1)";
           }, 20); /* 10 말고 20부터 잘작동 */
        },
        slideChangeTransitionEnd: function () {
            // 전환이 끝난 후 스케일 원래대로 되돌리기
            var activeSlide = this.slides[this.activeIndex];
            var slideContent = activeSlide.querySelector("img");
-           slideContent.style.transform = "rotate(0deg) scale(1.2)";
+           slideContent.style.transform = "rotate(0deg) scale(1)";
     
           // // 이전순서 슬라이드
           // var prevSlide = this.slides[this.previousIndex];

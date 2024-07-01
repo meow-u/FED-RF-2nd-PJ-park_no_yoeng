7f2aa9af7e0c9c1a84b73319ed23886c47da1892
meow-u <noyeong1994@gmail.com>
2024-07-02 03:31:16 +0900

    .

--- 2차프로젝트/03.구현소스/nypj-app/src/components/modules/make_itemList.jsx
+++ 2차프로젝트/03.구현소스/nypj-app/src/components/modules/make_itemList.jsx
@@ -1,17 +1,27 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
 // 상품데이터 불러오기(검색에서는 안씀)
-// import {allProducts} from "../data/products_data"
+import {allProducts} from "../data/products_data"
 import "../../css/_make_item_list.scss";
 
-function MakeItemList({ dt }) {
-   //dt - 검색된 배열데이터
+function MakeItemList({ dt ,menuTxt, isSub  }) {
+      //dt - 검색된 배열데이터
    //total - 검색된 배열데이터 개수
-   const total = dt.length;
+  console.log("menuTxt(=txt):", menuTxt);
+  console.log("isSub:", isSub);
+
+  useEffect(() => {
+    console.log("useEffect - menuTxt:", menuTxt); // useEffect 내에서 menuTxt 값을 확인
+    /* menuTxt값은 클릭해야 찍히도록 되어있음  */
+    let result = allProducts.filter((v) => v.collection === menuTxt);
+    console.log("result:", result);
+  });
+  let total;
+  if(dt) total = dt.length;
    console.log('상품뿌리는 나 랜더링! 데이터수:',total)
    return (
 <>
-     { total > 0 ?
+     { !menuTxt && !isSub && total > 0 ?
       <ul className="product-box">
          {dt.map((v, i) => (
                <li key={i}>
@@ -29,9 +39,36 @@ function MakeItemList({ dt }) {
                </li>
             ))}
       </ul> :
+       !menuTxt && !isSub && total === 0 &&
        <h2 className="nodata">
        Sorry, we don't have any matches for that search.
     </h2>}
+
+    <ul className="product-box">
+      {
+         allProducts
+            .filter((v) => isSub? v.collection === menuTxt: v.type === menuTxt) //txt는 전달받은 이너텍스트
+            .map((v, i) => (
+              <li key={i}>
+                <Link to={`/shop/product/${v.idx}`} state={{ itemIdx: v.idx }}>
+                  <img
+                    src={`${process.env.PUBLIC_URL}/images/${v.img}`}
+                    alt={`banner${i + 1}`}
+                  />
+                </Link>
+                <div className="item-wrap">
+                  <h2 className="etit">{v.name[1]}</h2>
+                  <h4 className="price">{v.price}원</h4>
+                  <h3 className="ktit">{v.name[0]}</h3>
+                  <p className="rev">review</p>
+                  <span className="rev2">{v.review}</span>
+                  <button className="item">Add to Cart</button>
+                </div>
+              </li>
+            ))
+      }
+    </ul>
+
       </>
    );
 }
--- 2차프로젝트/03.구현소스/nypj-app/src/components/pages/Shop.jsx
+++ 2차프로젝트/03.구현소스/nypj-app/src/components/pages/Shop.jsx
@@ -1,10 +1,91 @@
 // 펜할리곤스 Shop 서브페이지 컴포넌트
 
-export default function Shop() {
+import { useEffect, useState } from "react";
+import "../../css/_shop.scss";
+import { Link } from "react-router-dom";
+import MakeItemList from "../modules/make_itemList";
 
-   return (
-      <>
-         <h1>Shop</h1>
-      </>
-   );
-} //////////// Shop 컴포넌트 ////////
+export default function Shop() {
+  const [isSub, setIsSub] = useState(false);
+  const [txt, setTxt] = useState("");
+  //클릭단어가 컬렉션이 아닐경우 a.innerText를 할당
+
+  useEffect(() => {
+    let li = document.querySelectorAll("li");
+    console.log("모든 li(서브메뉴포함)", li);
+
+    li.forEach((el) =>
+      el.addEventListener("click", (e) => {
+        e.stopPropagation();
+        toggleCollection(el);
+      })
+    );
+  }, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 실행되도록 함
+
+  useEffect(() => {
+    // txt 값이 업데이트된 후에 필요한 작업을 수행
+    console.log("Updated txt:", txt);
+  }, [txt]); // txt 값이 변경될 때만 실행되도록 함
+
+  // 컬렉션 메뉴 토글 함수 ////////////////////////////////////
+  function toggleCollection(el) {
+    // 개별 li 클릭시 호출되는 함수( el은 개별 li)
+    let a = el.querySelector("a"); // 각 li안의 이너텍스트를 포함한 개별 a링크
+    let ol = el.querySelector("ol"); // 각 li안의 서브메뉴 ol 덩어리
+    let toSetText; //전역변수
+
+    if (a.innerText === "collection") {
+      // 컬렉션일때 높이토글  (하위보이게)
+      ol.style.height = ol.style.height === "21px" ? "0" : "21px";
+      // txt 값을 유지하므로 toSetText를 업데이트하지 않음
+    } else {
+      toSetText = a.innerText;
+      // 이부분이 else문에만 있어야함. if문 바깥에 있으면 collection을 클릭했을 때도 txt가 업데이트됨
+      setTxt(toSetText);
+      // 나머지 li 메뉴클릭시 내부 a의 이너텍스트값으로  'txt상태변수변경 '
+    }
+
+    setIsSub(el.parentElement.classList.contains("submenu"));
+    // 클릭된 각 li 부모 클래스가 submenu가 있으면 true (서브메뉴인지확인)
+  } ///////////////////////////////////////////////////// ///////
+
+  return (
+    <>
+      <ul className="menu-box">
+        <li>
+          <a href="###">FRAGRANCES</a>
+        </li>
+        <li>
+          <a href="###">GIFTING</a>
+        </li>
+        <li>
+          <a href="###">BATH &amp; BODY</a>
+        </li>
+        <li>
+          <a href="###">home</a>
+        </li>
+        <li>
+          <a href="###">collection</a>
+          <ol className="submenu">
+            <li>
+              <a href="###">British Tales</a>
+            </li>
+            <li>
+              <a href="###">Potions &amp; Remedies</a>
+            </li>
+            <li>
+              <a href="###">Trade Routes</a>
+            </li>
+            <li>
+              <a href="###">Portraits</a>
+            </li>
+          </ol>
+        </li>
+      </ul>
+      {/* 뿌리는컴포넌트 */}
+      <div className="gwrap">
+        <MakeItemList menuTxt={txt} isSub={isSub} />
+      </div>
+    </>
+  );
+}

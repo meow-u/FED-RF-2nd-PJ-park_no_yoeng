dcdb6d8eaa3e90d23f3c7df1857e86cde0097452
meow-u <noyeong1994@gmail.com>
2024-09-22 00:18:16 +0900

    Update ham.js

--- 2차프로젝트/03.구현소스/nypj-app/src/components/func/ham.js
+++ 2차프로젝트/03.구현소스/nypj-app/src/components/func/ham.js
@@ -121,12 +121,6 @@ const hamFn = () => {
         submenu.classList.remove("on"); // 서브 메뉴 숨기기
       });
       document.querySelector("body").style.overflow = "auto"; // 햄버거메뉴 닫혔을때 스크롤바 보이기
-
-      // 햄버거메뉴 닫힐때 포인트이벤트 초기화
-      $(".hambox>ul").css(
-        "pointer-events",
-        "all"
-      );
     } else {
       // 햄버거 메뉴가 열릴 때
       document.querySelector(".tmenu.col-4").style.opacity = "0"; // 상단 메뉴 숨기기
@@ -135,6 +129,8 @@ const hamFn = () => {
       setTimeout(() => {
         document.querySelector("body").style.overflow = "hidden";
       }, 100); // 햄버거메뉴 열렸을때 스크롤바 없애기
+      // 햄버거메뉴 열릴때 포인트이벤트 초기화
+      $(".hambox>ul").css("pointer-events", "all");
     }
   }
 
